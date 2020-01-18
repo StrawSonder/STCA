5284dbe0bd658b8dc460044af0c52af72429ab0c
Leo Zhang <leohzhang11@gmail.com>
2020-01-18 02:17:39 -0600

    Added comments, changed rank.js so that it uses helper methods

--- Rank.js
+++ Rank.js
@@ -1,100 +1,140 @@
 //https://regex101.com/r/OrmORY/1
 
-class Rank {
-    calcTotal(TC, SZ, RM, CB) {
-        var arr = [TC, SZ, RM, CB];
-        total = 0;
-        for (var i = 0; i < 4; i++) {
-            var str = checkRank(arr[i]);
-            if (!isEmpty(str)) {
-                return str;
-            }
-        }
-        return "";
+/**
+ * Calculates the total associated with those ranks
+ * @param rank the rank associated with the mode
+ * @return str if there is an error, the associated total if no error
+ */
+calcTotal = (TC, SZ, RM, CB) => {
+    var arr = [TC, SZ, RM, CB];
+    var total = 0;
+    var str;
+    for (var i = 0; i < 4; i++) {
+        str = checkRank(arr[i]);
+        if (typeof str === "string") return str;
+        else total += str;
     }
+    return total;
+}
+
+/**
+ * Checks if the rank is valid
+ * @param rank the rank to be checked
+ * @return str if there is an error, the associated total if no error
+ */
+checkRank = (rank) => {
+    var total = 0;
+    if (rank === "C-") total += 0.5;
+    else if (rank === "C") total += 1;
+    else if (rank === "C+") total += 1.5;
+
+    else if (rank === "B-") total += 2;
+    else if (rank === "B") total += 2.5;
+    else if (rank === "B+") total += 3;
+
+    else if (rank === "A-") total += 3.5;
+    else if (rank === "A") total += 4;
+    else if (rank === "A+") total += 4.5;
+
+    else if (rank === "S") total += 5;
+
+    else if (rank === "S+0") total += 5.5;
+    else if (rank === "S+1") total += 5.5;
+    else if (rank === "S+2") total += 5.5;
+    else if (rank === "S+3") total += 6;
+    else if (rank === "S+4") total += 6;
+    else if (rank === "S+5") total += 6;
+    else if (rank === "S+6") total += 6.5;
+    else if (rank === "S+7") total += 6.5;
+    else if (rank === "S+8") total += 6.5;
+    else if (rank === "S+9") total += 7;
+
+    else if (rank === "X") total += 8;
     
-    calcTotal2() {
-        return calcTotal(TC, SZ, RM, CB);
-    }
+    else if (rank === "S+") return "Please add a number after S+.";
+    else return "Those are not correct rankings! Correct usage is:\n!!register [TC] [SZ] [RM] [CB]\n"
+        + "ex. !!register C B A S";
+    return total;
+}
 
-    checkRank(rank) {
-        if (rank === "C-") total += 0.5;
-        else if (rank === "C") total += 1;
-        else if (rank === "C+") total += 1.5;
+/**
+ * Gets the role associated with the total
+ * @param total the total to be used for calculation
+ * @return role the role that should be given to the user that has that total
+ */
+getYear = (total) => {
+    var freshman = message.guild.roles.find(role => role.name === "Freshman");
+    var sophomore = message.guild.roles.find(role => role.name === "Sophomore");
+    var junior = message.guild.roles.find(role => role.name === "Junior");
+    var senior = message.guild.roles.find(role => role.name === "Senior");
+    if (total < 10 ) role = freshman;
+    else if (total < 20) role = sophomore;
+    else if (total < 30) role = junior;
+    else role = senior;
+    return role;
+}
 
-        else if (rank === "B-") total += 2;
-        else if (rank === "B") total += 2.5;
-        else if (rank === "B+") total += 3;
-
-        else if (rank === "A-") total += 3.5;
-        else if (rank === "A") total += 4;
-        else if (rank === "A+") total += 4.5;
-
-        else if (rank === "S") total += 5;
-
-        else if (rank === "S+0") total += 5.5;
-        else if (rank === "S+1") total += 5.5;
-        else if (rank === "S+2") total += 5.5;
-        else if (rank === "S+3") total += 6;
-        else if (rank === "S+4") total += 6;
-        else if (rank === "S+5") total += 6;
-        else if (rank === "S+6") total += 6.5;
-        else if (rank === "S+7") total += 6.5;
-        else if (rank === "S+8") total += 6.5;
-        else if (rank === "S+9") total += 7;
-
-        else if (rank === "X") total += 8;
-        
-        else if (rank === "S+") return "Please add a number after S+.";
-        else return "Those are not correct rankings! Correct usage is:\n!!register [TC] [SZ] [RM] [CB]\n"
-            + "ex. !!register C B A S";
-        return "";
-    }
-
-    getYear() {
-        var freshman = message.guild.roles.find(role => role.name === "Freshman");
-        var sophomore = message.guild.roles.find(role => role.name === "Sophomore");
-        var junior = message.guild.roles.find(role => role.name === "Junior");
-        var senior = message.guild.roles.find(role => role.name === "Senior");
-        if (total < 10 ) {
-            roleName = "freshman";
-            role = freshman;
-        } else if (total < 20) {
-            roleName = "sophomore";
-            role = sophomore;
-        } else if (total < 30) {
-            roleName = "junior";
-            role = junior;
-        } else {
-            roleName = "senior";
-            role = senior;
-        }
-    }
-
-    set(mode, rank) {
-        mode = mode.toUpperCase();
-        var str = checkRank(rank);
-        if (!isEmpty(str)) return str;
-        if (mode === "TC") TC = rank; 
-        else if (mode === "SZ") SZ = rank;
-        else if (mode === "RM") RM = rank;
-        else if (mode === "CB") CB = rank;
-        else return "Invalid mode.";
-        calcTotal2();
-        return "";
-    }
+/**
+ * Rank object contains information about the ranks for the modes of the user as well as the user id
+ */
+class Rank {
+    
+    /**
+     * Instantiates a rank object, except when there is an error
+     * @param {tower control} TC 
+     * @param {splat zones} SZ 
+     * @param {rainmaker} RM 
+     * @param {clam blitz} CB 
+     * @param {message sent by the user} message 
+     * @return str if there is an error, will have the associated error message
+     */
     constructor(TC, SZ, RM, CB, message) {
-        this.total = 0;
-        this.roleName = "";
-        this.role;
-        var str = calcTotal(TZ, SZ, RM, CB);
-        if (!isEmpty(str)) return str;
+        var str = calcTotal(TC, SZ, RM, CB);
+        if (typeof str === "string") return str;
+        else this.total = str;
+        this.role = getYear(this.total);
         this.TC = TC;
         this.SZ = SZ;
         this.RM = RM;
         this.CB = CB;
         this.id = message.member.id;
-        getYear();
+    }
+    
+    /**
+     * Sets the total if there is no error
+     * @return total if no error, str if error
+     */
+    setTotal() {
+        var str = calcTotal(TC, SZ, RM, CB);
+        if (typeof str === "string") return str;
+        total = str;
+        return str;
+    }
+
+    /**
+     * Sets the class of the student (freshman, sophomore, etc.)
+     */
+    setYear() {
+        role = getYear(total);
+    }
+
+    /**
+     * Sets a specific mode's rank using user's data from ranks array
+     * @param {Mode that user is setting} mode 
+     * @param {Rank to be setting the mode's rank to} rank 
+     */
+    set(mode, rank) {
+        mode = mode.toUpperCase();
+        rank = rank.toUpperCase();
+        var str = checkRank(rank);
+        if (typeof str === "string") return str;
+        if (mode === "TC") TC = rank; 
+        else if (mode === "SZ") SZ = rank;
+        else if (mode === "RM") RM = rank;
+        else if (mode === "CB") CB = rank;
+        else return "Invalid mode.";
+        setTotal();
+        return total;
     }
 }
 
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-
+//Iniitalization
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const {token, prefix} = require('./config.json')
+//Adding Rank class and methods
 var Rank = require('./Rank.js');
+//Array for saving ranks
 var ranks = {};
 
 var express = require('express');
-var app     = express();
+var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -22,17 +24,30 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 })
 
+/**
+ * When new user joins, adds Visitor role
+ * Sends welcome message
+ */
 client.on('guildMemberAdd', (guildMember) => {
     guildMember.addRole(guildMember.guild.roles.find(role => role.name === "Visitor"));
     console.log(`${guildMember.user.tag} has gotten the visitor role.`);
     member.guild.channels.get('667773048732254244').send(`Welcome to the STCA, <@` + member.id + `>! If you are a student, head over to #registration !`); 
 })
 
+/**
+ * When user leaves, sends leaving message
+ */
 client.on('guildMemberRemove', member => {
     member.guild.channels.get('667773048732254244').send(`${member.user.tag} just left the server!`); 
 });
 
-//Dealing with registration roles
+/**
+ * Adds new class (freshman, sophomore, etc.) role to user
+ * Used for registration as well as graduating to a higher class
+ * @param message the message sent by the user
+ * @param arr the array of roles
+ * @param add the role to be added
+ */
 registration = (message, arr, add) => {
     for (var i = 0; i < arr.length; i++) {
         if (arr[i] === add) {
@@ -43,6 +58,10 @@ registration = (message, arr, add) => {
     }
 }
 
+/**
+ * Run when bot sees a message
+ * Mostly for commands
+ */
 client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     const cannotUse = "I'm sorry, you cannot use that command.";
@@ -61,8 +80,14 @@ client.on('message', message => {
     var teacher = message.guild.roles.find(role => role.name === "Teacher");
     var offline = message.guild.roles.find(role => role.name === "Offline Teacher");
 
+    /**
+     * Registration command
+     * used for first time students to get a class
+     * Also saves the ranks to the ranks array
+     */
     if (command === 'register') {
-        var arr, member, name, possessive, roleName;
+        var arr, member, name, possessive;
+        //only admins have the power to add roles to others
         if (args.length == 5 && message.member.hasPermission("ADMINISTRATOR")) {
             arr = args.slice(1);
             member = message.mentions.members.first();
@@ -72,6 +97,7 @@ client.on('message', message => {
             }
             name = member.user.username + " is";
             possessive = member.user.username + "'s";
+        //anyone can register themselves
         } else if (args.length == 4) {
             arr = args;
             name = "You are";
@@ -81,19 +107,28 @@ client.on('message', message => {
             return;
         }
         
+        //arr is the array of ranks: [TC], [SZ], [RM], [CB]
         if (args.length == 5) arr = args.slice(1);
         for (var i = 0; i < 4; i++) arr[i] = arr[i].toUpperCase();
+        //rank variable contains info about the ranks and the user that the ranks are associated with
         var rank = new rank(arr[0], arr[1], arr[2], arr[3], message);
+        //in general, errors are indicated by the method returning a string associated with that error
+        //instead of the object or primitive it was supposed to return
         if (typeof rank === "string") {
             message.channel.send(rank);
             return;
         }
+        //if valid rank, add it to the array
         ranks.push(rank);
         registration(message, roles, rank.role);
         message.channel.send(greeting + possessive + " ranks are "
             + arr[0] + " for tower control, " + arr[1] + " for splat zones, "
             + arr[2] + " for rainmaker, and " + arr[3] + " for clam blitz.\n"
             + name + " now a " + rank.roleName + " of the academy!");
+    /**
+     * checkin command allows offline teachers to check in
+     * Offline Teacher is unpingable, while Teacher is pingable
+     */
     } else if (command === 'checkin') {
         if (message.member.roles.find("name", "Offline Teacher")) {
             message.channel.send("You have successfully checked in.")
@@ -128,23 +163,28 @@ client.on('message', message => {
                 lastMessage.delete(timeout);
             }
         }).catch(console.error);
+    /**
+     * Updates one rank at a time for students that have already registered
+     */
     } else if (command === "update") {
         if (args.length != 2) message.channel.send("Incorrect usage! Correct usage is:\n"
             + "!!update [mode] [rank]\nex. !!update TC S");
+        //finds the rank object associated with the user
         var ind = ranks.findIndex(rank => rank.id === message.member.id);
         if (ind == undefined) message.channel.send("You must register first!\n"
-            + "If you have already registered, contact an admin. Data has been deleted.");
+            + "If you had previously registered and are getting this error, ping @Straw or @leonidasxlii.");
+        //sets the specific rank
         var str = ranks[ind].set(args[0], args[1]);
-        if (!isEmpty(str)) return str;
-        var old = ranks[ind].roleName;
-        ranks[ind].getYear();
+        if (typeof str === "string") return str;
+        var old = ranks[ind].role;
+        ranks[ind].setYear();
         var add = "";
-        if (old !== ranks[ind].roleName) {
-            add = "\nYou are now a " + ranks[ind].roleName + "! Congratulations!";
+        //checks if user should have changed a class
+        if (old !== ranks[ind].role) {
+            add = "\nYou are now a " + ranks[ind].role.name + "! Congratulations!";
             registration(message, roles, rank.role);
         }
-        message.channel.send(greeting + "Your " + args[0] + " rank is now " + args[1]
-            + "." + add);
+        message.channel.send(greeting + "Your " + args[0] + " rank is now " + args[1] + "." + add);
     } else {
         message.channel.send("I do not recognize that command!");
     }

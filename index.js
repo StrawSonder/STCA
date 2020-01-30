5d8bb10b8e276b6499df9fe7177aceb5250fb52a
Leo Zhang <leohzhang11@gmail.com>
2020-01-30 14:35:58 -0500

    Updated JP role, deleting messages for roles now as well

--- index.js
+++ index.js
@@ -255,15 +255,19 @@ client.on('message', message => {
         } 
         var role = guild.roles.find(role => role.name === args[0].toUpperCase())
         if (role != undefined) {
+            if (args[0] != "SW" && "LFG" && "NA" && "EU" && "JP") {
+                message.channel.send("I'm sorry! You cannot use that role!");
+                return;
+            }
             if (message.member.roles.find(ele => ele.name === role.name)) {
                 message.member.removeRole(role);
                 message.channel.send(`Successfully removed the role ${role.name}!`)
             } else {
                 message.member.addRole(role);
                 message.channel.send(`Successfully added the role ${role.name}!`)
-            } 
-        } else if (args[0] != "SW" && "LFG" && "NA" && "EU") message.channel.send("I'm sorry! You cannot use that role!");
-        else message.channel.send("That is not a role!");
+            }
+            deleteMessages(message); 
+        } else message.channel.send("That is not a role!");
     } else if (command === 'start') {
         if (!message.member.hasPermission("ADMINISTRATOR")) {
             message.channel.send(cannotUse);
